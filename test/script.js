6dff3bb35d99d03ac4e0fb50c037fa071ed17da4
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2023-09-06 18:34:35 -0300

    c

--- test/script.js
+++ test/script.js
@@ -1,10 +1,15 @@
 // Obtén la referencia al elemento de la caja
 const caja = document.getElementById('caja');
+const proyectil = document.getElementById('proyectil');
 
 // Posición inicial de la caja
 let posX = 0;
 let posY = 0;
 const step = 10; // Cantidad de píxeles para mover la caja en cada paso
+
+const proyectilSpeed = 5; // Velocidad del proyectil
+let disparoActivo = false; // Variable para controlar si se puede disparar
+
 
 // Función para mover la caja
 function moverCaja(event) {
@@ -21,6 +26,34 @@ function moverCaja(event) {
   // Actualiza la posición de la caja
   caja.style.left = posX + 'px';
   caja.style.top = posY + 'px';
+
+  // Agregar lógica para disparar
+  if (event.key.trim()=== 'Space' && !disparoActivo) {
+    dispararProyectil();
+  }
+}
+
+function dispararProyectil() {
+  disparoActivo = true;
+  proyectil.style.display = 'block';
+  proyectil.style.left = posX + 'px';
+  proyectil.style.top = posY + 'px';
+
+  // Función para mover el proyectil
+  function moverProyectil() {
+    const proyectilX = parseInt(proyectil.style.left);
+    proyectil.style.left = proyectilX + proyectilSpeed + 'px';
+
+    // Verificar si el proyectil ha salido de la pantalla
+    if (proyectilX > window.innerWidth) {
+      proyectil.style.display = 'none';
+      disparoActivo = false;
+      clearInterval(intervaloProyectil);
+    }
+  }
+
+  // Iniciar un intervalo para mover el proyectil
+  const intervaloProyectil = setInterval(moverProyectil, 10);
 }
 
 // Agrega un evento de escucha para las teclas de flecha
